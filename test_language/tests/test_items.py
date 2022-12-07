06a1d791438d70af8ca40bdad2f8614d25901b55
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2022-12-07 04:09:35 +0300

    task_language

--- test_language/tests/test_items.py
+++ test_language/tests/test_items.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from time import sleep
 
 
 def test_add_to_cart_button(driver, language_options):
@@ -9,6 +8,4 @@ def test_add_to_cart_button(driver, language_options):
     add_to_cart_button = driver.find_element(
         By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket'
     )
-    british_language = driver.find_element(By.CSS_SELECTOR, 'select [value="en-gb"]')
-    sleep(5)
     assert add_to_cart_button.is_displayed()
